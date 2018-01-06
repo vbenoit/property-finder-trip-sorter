0f4f331a1dd840595668045fee598c34eb684942
vbenoit <vincent.benoit.site.web@gmail.com>
2018-01-06 11:06:20 +0100

    refacto - pathdfinding service + tests

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ helpers = {
 		}
 		return 0;
 	}
-}
+};
 
 Array.prototype.contains = function( contained ) {
 
@@ -61,6 +61,152 @@ Array.prototype.contains = function( contained ) {
 
 };
 
+itineraryService = {
+
+	helpers: helpers,
+
+	/*2- recursive algorithm to two find all paths from a city to another city without
+		passing by the same place two times
+	*/
+	findAllPathsFromAToB: function( src, dest, excludedPoints, neighboursMap, containerObject ) {
+
+		if (!containerObject.correctPaths) {
+			containerObject.correctPaths = [];
+		}
+
+		//dest in direct src neighbors
+		if ( neighboursMap[src].indexOf(dest) >= 0 ) {
+			//direct transportation
+			if ( excludedPoints.length === 0 ){
+				containerObject.correctPaths.push( [ src, dest ] );
+			}
+			else{
+				return [ src , dest ];
+			}
+		}
+		//all current neighbors excluded
+		if ( excludedPoints.contains( neighboursMap[src] ) ) {
+			return null;
+		}
+
+		for ( var i = 0; i < neighboursMap[src].length; i ++ ) {
+			//current neighbor not excluded
+			if ( excludedPoints.indexOf( neighboursMap[src][ i ] ) < 0  ){
+				var updatedExcludedPoints = 
+					excludedPoints.indexOf(src) < 0 ? excludedPoints.concat([src]) : excludedPoints;
+				var path = this.findAllPathsFromAToB( 
+					neighboursMap[src][ i ], dest, updatedExcludedPoints, neighboursMap, containerObject );
+				if ( path ){
+					var result;
+					if ( excludedPoints.length == 0 ){
+						result = [src].concat(path);
+					}
+					else{ 
+						result = updatedExcludedPoints.concat( path );
+					}
+					containerObject.correctPaths.push( result );
+				}
+			}
+		}
+
+	},
+	getBestItinerary: function( correctPaths, sortByPrice, sortByTime, cacheContainer, travelsData ) {
+
+		var smallestAmount = null;
+		var bestItineraryIndex = null;
+		var bestItinerary = null;
+
+		for ( var i=0, l = correctPaths.length; i < l; i++ ) {
+			currentAmount = 0;
+			var currentItinerary = [];
+			var nullTransportation = false;
+			for ( var j=0; j < correctPaths[i].length - 1; j++ ){
+				
+				var bestTransportation = null;
+
+				if ( sortByPrice ) { 
+					bestTransportation = 
+						this.getBestTransportation( 
+							correctPaths[i][j], 
+							correctPaths[i][j + 1],
+							travelsData,
+							cacheContainer,
+							"cheapestTransportations",
+							"discountedCost"
+						);
+				}
+				else if ( sortByTime ) { 
+					bestTransportation = 
+						this.getBestTransportation( 
+							correctPaths[i][j], 
+							correctPaths[i][j + 1],
+							travelsData,
+							cacheContainer,
+							"fastestTransportations",
+							"travelTime"
+						);
+				}
+
+				if (!bestTransportation){
+					nullTransportation = true;
+					break;
+				}
+
+				if ( sortByPrice ) { 
+					currentAmount += bestTransportation.discountedCost;
+				}
+				if ( sortByTime ){
+					currentAmount += bestTransportation.travelTime
+				}
+				currentItinerary.push( new Transportation(bestTransportation) );
+			}
+
+			if ( nullTransportation ){
+				continue;
+			}
+
+			if ( smallestAmount == null || currentAmount < smallestAmount ){
+				smallestAmount = currentAmount;
+				bestItineraryIndex = i;
+				bestItinerary = this.helpers.deepCopy( currentItinerary );
+			}
+		}
+
+		return bestItinerary;
+
+	},
+	getBestTransportation: function( pDeparture, pArrival, travelsData, cacheContainer, cachePropertyName, propertyToCompare ) {
+			
+		//a cache system
+		if ( !cacheContainer[cachePropertyName][pDeparture + "|" + pArrival] ) {
+
+			var directTransportations = this.getDirectTransportations( pDeparture, pArrival, travelsData );
+			var bestDeal = null;
+			//the best(cheapest/fastest) transportation
+			for ( var i= 0; i < directTransportations.length; i++ ) {
+				var currentTransportation = new Transportation( directTransportations[i] ); 
+				currentTransportation.computeEffectiveCost();
+				currentTransportation.computeTravelTimeInMin();
+
+				if ( !bestDeal || currentTransportation[propertyToCompare] < bestDeal[propertyToCompare] ){
+					bestDeal = this.helpers.deepCopy( currentTransportation );
+				}
+			}
+			//fill the cache with the computed value
+			cacheContainer[cachePropertyName][pDeparture + "|" + pArrival] = this.helpers.deepCopy( bestDeal );
+		}
+
+		return cacheContainer[cachePropertyName][pDeparture + "|" + pArrival];
+
+	},
+	getDirectTransportations: function( pDeparture, pArrival, travelsData ) {
+		return travelsData.deals.filter( function( travel ) {
+			return ( travel.departure === pDeparture && travel.arrival === pArrival  );
+		} );
+	}
+
+};
+
 app = new Vue({
 	http: Vue.http,
 
@@ -69,6 +215,7 @@ app = new Vue({
 		return {
 
 			helpers: helpers,
+			pItineraryService: itineraryService,
 
 			sortByPrice: true,
 			sortByTime: false,
@@ -168,6 +315,22 @@ app = new Vue({
 			this.sortByTime = false;
 			this.sortByPrice = true;
 		},
+		displayError: function( msg ) {
+			this.errorMessage = msg;
+		},
+		launchSearchFormControl: function() {
+			var bothValuesSet = this.currentDeparture != "0" && this.currentArrival != "0";
+			var bothValuesDifferent = ( this.currentDeparture != this.currentArrival );
+
+			if ( !bothValuesSet ) {
+				this.displayError( this.BOTH_VALUES_SET_MSG );
+			}
+
+			if ( !bothValuesDifferent ) {
+				this.displayError( this.BOTH_VALUES_DIFFERENT_MSG );
+			}
+			return (bothValuesSet && bothValuesDifferent);
+		},
 		launchSearch: function() {
 
 			/* djikstras algorithm doesn't seem to be the most 
@@ -177,132 +340,30 @@ app = new Vue({
 			/*  init results paths */
 			this.correctPaths = [];	
 
-			var bothValuesSet = this.currentDeparture != "0" && this.currentArrival != "0";
-			var bothValuesDifferent = ( this.currentDeparture != this.currentArrival );
+			var formOk = this.launchSearchFormControl();
 
-			if ( !bothValuesSet ) {
-				this.errorMessage = this.BOTH_VALUES_SET_MSG;
+			if (!formOk){
 				return;
 			}
 
-			if ( !bothValuesDifferent ) {
-				this.errorMessage = this.BOTH_VALUES_DIFFERENT_MSG;
-				return;
-			}
-
-			if ( bothValuesSet && bothValuesDifferent ) {
-				ways= this.findAllPathsFromAToB( this.currentDeparture, this.currentArrival, []);
-				this.correctPaths = this.correctPaths.concat( [ways] );
-			}
-
-			console.log( this.correctPaths.length + " - before removing duplicates" );
+			this.correctPaths = 
+				this.pItineraryService.findAllPathsFromAToB( 
+					this.currentDeparture, this.currentArrival, [], this.neighboursMap, this);
 
 			/* 2 - remove duplicates */
 			this.correctPaths = this.helpers.removeDuplicates(this.correctPaths);
 
-			console.log( this.correctPaths.length + " - after removing duplicates" );
-
 			/* 3 - we chose the cheapest/fastest */
-			var bestItinerary = this.getBestItinerary( this.correctPaths, this.sortByPrice, this.sortByTime );
+			var bestItinerary = 
+				this.pItineraryService.getBestItinerary( 
+					this.correctPaths, this.sortByPrice, this.sortByTime, this, this.travelsData );
 			
 			this.processedItinerary = new Itinerary( bestItinerary );
 			this.processedItinerary.computeAdditionalInformations();
 			console.timeEnd("Launchsearch processing time");
 
 			this.displayResult = true;
-			
-			/* processing for additional informations for an itinerary */
 
-		},
-		getBestItinerary: function( correctPaths, sortByPrice, sortByTime ) {
-
-			var smallestAmount = null;
-			var bestItineraryIndex = null;
-			var bestItinerary = null;
-
-			for ( var i=0, l = correctPaths.length; i < l; i++ ) {
-				currentAmount = 0;
-				var currentItinerary = [];
-				var nullTransportation = false;
-				for ( var j=0; j < correctPaths[i].length - 1; j++ ){
-					
-					var bestTransportation = null;
-
-					if ( sortByPrice ) { 
-						bestTransportation = 
-							this.getBestTransportation( 
-								correctPaths[i][j], 
-								correctPaths[i][j + 1],
-								"cheapestTransportations",
-								"discountedCost"
-							);
-					}
-					else if ( sortByTime ) { 
-						bestTransportation = 
-							this.getBestTransportation( 
-								correctPaths[i][j], 
-								correctPaths[i][j + 1],
-								"fastestTransportations",
-								"travelTime"
-							);
-					}
-
-					if (!bestTransportation){
-						nullTransportation = true;
-						break;
-					}
-
-					if ( sortByPrice ) { 
-						currentAmount += bestTransportation.discountedCost;
-					}
-					if ( sortByTime ){
-						currentAmount += bestTransportation.travelTime
-					}
-					currentItinerary.push( new Transportation(bestTransportation) );
-				}
-
-				if ( nullTransportation ){
-					continue;
-				}
-
-				if ( smallestAmount == null || currentAmount < smallestAmount ){
-					smallestAmount = currentAmount;
-					bestItineraryIndex = i;
-					bestItinerary = this.helpers.deepCopy( currentItinerary );
-				}
-			}
-
-			return bestItinerary;
-
-		},
-		getBestTransportation: function( pDeparture, pArrival, cachePropertyName, propertyToCompare ) {
-			
-			//a cache system
-			if ( !this[cachePropertyName][pDeparture + "|" + pArrival] ) {
-
-				var directTransportations = this.getDirectTransportations( pDeparture, pArrival );
-				var bestDeal = null;
-				//the best(cheapest/fastest) transportation
-				for ( var i= 0; i < directTransportations.length; i++ ) {
-					var currentTransportation = new Transportation( directTransportations[i] ); 
-					currentTransportation.computeEffectiveCost();
-					currentTransportation.computeTravelTimeInMin();
-
-					if ( !bestDeal || currentTransportation[propertyToCompare] < bestDeal[propertyToCompare] ){
-						bestDeal = this.helpers.deepCopy( currentTransportation );
-					}
-				}
-				//fill the cache with the computed value
-				this[cachePropertyName][pDeparture + "|" + pArrival] = this.helpers.deepCopy( bestDeal );
-			}
-
-			return this[cachePropertyName][pDeparture + "|" + pArrival];
-
-		},
-		getDirectTransportations: function( pDeparture, pArrival ) {
-			return this.travelsData.deals.filter( function( travel ) {
-				return ( travel.departure === pDeparture && travel.arrival === pArrival  );
-			} );
 		},
 		generateMap: function() {
 
@@ -322,46 +383,7 @@ app = new Vue({
 				}
 			}
 		},
-		/*2- recursive algorithm to two find all paths from a city to another city without
-			passing by the same place two times
-			*/
-		findAllPathsFromAToB: function( src, dest, excludedPoints ) {
-
-			//dest in direct src neighbors
-			if ( this.neighboursMap[src].indexOf(dest) >= 0 ) {
-				//direct transportation
-				if ( excludedPoints.length === 0 ){
-					this.correctPaths.push( [ src, dest ] );
-				}
-				else{
-					return [ src , dest ];
-				}
-			}
-			//all current neighbors excluded
-			if ( excludedPoints.contains( this.neighboursMap[src] ) ) {
-				return null;
-			}
-
-			for ( var i = 0; i < this.neighboursMap[src].length; i ++ ) {
-				//current neighbor not excluded
-				if ( excludedPoints.indexOf( this.neighboursMap[src][ i ] ) < 0  ){
-					var updatedExcludedPoints = 
-						excludedPoints.indexOf(src) < 0 ? excludedPoints.concat([src]) : excludedPoints;
-					var path = this.findAllPathsFromAToB( this.neighboursMap[src][ i ], dest, updatedExcludedPoints );
-					if ( path ){
-						var result;
-						if ( excludedPoints.length == 0 ){
-							result = [src].concat(path);
-						}
-						else{ 
-							result = updatedExcludedPoints.concat( path );
-						}
-						this.correctPaths.push( result );
-					}
-				}
-			}
-
-		}
+		
 	},
 	mounted: function(){
 		this.getTravelsData();
@@ -433,13 +455,28 @@ function tests(){
 
 	function processItinerary( pDeparture, pArrival, sortByPrice, sortByTime ) {
 
-		var ways= app.findAllPathsFromAToB( pDeparture, pArrival, []);
-		var correctPaths = helpers.deepCopy( app.correctPaths );
+		var correctPathsContainerObject = {};
+		var neighboursMap = app.neighboursMap;
 
-		correctPaths = helpers.removeDuplicates(correctPaths);
+		itineraryService.findAllPathsFromAToB( 
+			pDeparture, pArrival, [], neighboursMap, correctPathsContainerObject);
+
+		correctPaths = helpers.removeDuplicates(correctPathsContainerObject.correctPaths);
+
+		correctPathsContainerObject = null;
+
+		var cacheContainer = {};
+		cacheContainer.cheapestTransportations= [];
+		cacheContainer.fastestTransportations= [];
+
+		var travelsData = app.travelsData;
 
 		var processedItinerary = 
-			new Itinerary( app.getBestItinerary( correctPaths, sortByPrice, sortByTime ) );
+			new Itinerary( 
+				itineraryService
+					.getBestItinerary( 
+						correctPaths, sortByPrice, sortByTime, cacheContainer, travelsData ) );
+
 		processedItinerary.computeAdditionalInformations();	
 
 		return processedItinerary;
